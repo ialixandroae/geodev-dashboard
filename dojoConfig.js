936381efa8c0ad15082f92083ab7e159d6669bc8
Ionut Alixandroae <ialixandroae@yahoo.com>
2018-05-24 22:19:11 +0300

    Update dojoConfig.js

--- dojoConfig.js
+++ dojoConfig.js
@@ -8,10 +8,6 @@ window.dojoConfig = {
             main: 'main'
         }, {
             name: 'app',
-            location: 'http://127.0.0.1:8081' + '/app',
-            main: 'main'
-        }, {
-            name: 'app',
             location: 'https://ialixandroae.github.io/geodev-dashboard/' + '/app',
             main: 'main'
         }]
